7897a82c420096621bb8e9d4f00db70f9ec70994
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2020-06-04 15:50:12 +0300

    Add backpack activity

---
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/BackpackActivity.kt
@@ -0,0 +1,11 @@
+package com.makentoshe.androidgithubcitemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BackpackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_backpack)
+    }
+}
