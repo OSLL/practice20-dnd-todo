6bdf6f127ee5fbf9ca016b4f52aa4b2df9fcbce8
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2020-06-04 15:47:29 +0300

    Add game activity

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/GameActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/GameActivity.kt
@@ -6,5 +6,6 @@ import android.os.Bundle
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
     }
 }
