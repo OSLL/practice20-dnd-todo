b2aa8b517a8be9de4fe0c0e83ec3700fc4109ed9
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2020-06-04 16:17:40 +0300

    Add switching to backpack

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/GameActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/GameActivity.kt
@@ -1,11 +1,23 @@
 package com.makentoshe.androidgithubcitemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        backpack.setOnClickListener { v: View? ->
+            val intent: Intent = Intent(this, BackpackActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onBackPressed() {
+
     }
 }
