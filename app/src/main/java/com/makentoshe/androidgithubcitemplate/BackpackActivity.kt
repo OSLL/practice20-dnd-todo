7195227e69d40948a2f0977805ff73cb976b7a8b
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2020-06-04 16:23:39 +0300

    Add back button

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/BackpackActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/BackpackActivity.kt
@@ -2,10 +2,15 @@ package com.makentoshe.androidgithubcitemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_backpack.*
 
 class BackpackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_backpack)
+
+        back.setOnClickListener {
+            super.onBackPressed()
+        }
     }
 }
