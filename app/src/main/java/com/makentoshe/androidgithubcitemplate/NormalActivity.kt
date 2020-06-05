28bebcf1265806e71c34e4076f3fb40ab3f93085
Alexander Podkopaev <alexander@podkopaev.net>
2020-06-05 13:38:08 +0300

    Corrected foundation 2.0

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/NormalActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/NormalActivity.kt
@@ -6,16 +6,21 @@ import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_normal.*
 
-class MainActivity : AppCompatActivity() {
-
+class NormalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        setContentView(R.layout.activity_main)
-        start.setOnClickListener {
-            Log.d("tag", "Start")
-            val myIntent = Intent(this, NormalActivity::class.java)
+        setContentView(R.layout.activity_normal)
+        exit.setOnClickListener {
+            Log.d("tag", "End")
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+        }
+        equipment.setOnClickListener {
+            Log.d("tag", "Equipment")
+            val myIntent = Intent(this, Equipment::class.java)
             startActivity(myIntent)
         }
     }
