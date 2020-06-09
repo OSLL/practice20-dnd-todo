0535e53706f9f6c823781d4e41a6ff584686f8a6
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2020-06-09 17:38:26 +0300

    Refactor

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/Item.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/Item.kt
@@ -9,6 +9,7 @@ data class Item (
     @PrimaryKey(autoGenerate = true) var iid: Int = 0,
     @ColumnInfo(name = "drawable_id") var drawableId: Int = 0,
     @ColumnInfo(name = "type_of_item") var typeOfItem: String = "",
-    @ColumnInfo(name = "extend_hp") var extendHp: Int = 0,
+    @ColumnInfo(name = "armor") var armor: Int = 0,
+    @ColumnInfo(name = "attack") var attack: Int = 0,
     @ColumnInfo(name = "equiped") var equiped: Boolean = false
 )
