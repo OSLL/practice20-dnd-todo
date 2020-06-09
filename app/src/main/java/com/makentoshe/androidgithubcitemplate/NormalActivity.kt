3b878d1de832e1c94c4ce21c2aa1ac7b37be0af5
Alexander Podkopaev <alexander@podkopaev.net>
2020-06-09 10:20:02 +0300

    Added prologue

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/NormalActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/NormalActivity.kt
@@ -10,12 +10,29 @@ import kotlinx.android.synthetic.main.activity_normal.*
 
 class NormalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        var pr : Int = 1
+        var prolog = Array<String>(4, {i : Int -> i.toString()})
+        prolog[0] = "Однажды, в одном городе появился странник в плаще. Он был похож на обычного крестьянина, но аура у него была, как у солдата."
+        prolog[1] = "Он пришел в бар, где заказал крепкое пиво и начал расспрашивать про замок. Он сказал, что он искатель приключений, а также сказал, что слышал про здешних монстров в замке."
+        prolog[2] = "Странник сказал, что он победит монстров, но не просто так. Горожане предложили ему стать королем здешнего королевства, если он победит короля монстров."
+        prolog[3] = "Странник согласился, после чего взял нужные предметы и отправился в замок"
+        var monster = Array<String>(5, {i : Int -> i.toString()})
+        monster[0] = ""
+        monster[1] = ""
+        monster[2] = ""
+        monster[3] = ""
+        monster[4] = ""
         super.onCreate(savedInstanceState)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_normal)
 
+        text.text = prolog[0]
+
         text.setOnClickListener {
-            text.text = "AAAAAAA"
+            if (pr < 4) {
+                text.text = prolog[pr]
+                pr++
+            }
         }
 
         exit.setOnClickListener {
