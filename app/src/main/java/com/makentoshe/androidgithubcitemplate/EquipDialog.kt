3afa9366d45333714f2464145aaacc786073561a
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2020-06-09 13:34:15 +0300

    Fix dialog issue

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/EquipDialog.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/EquipDialog.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
@@ -14,6 +15,7 @@ class EquipDialog : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(requireContext())
+        //Log.d("DIALOG CONTEXT", requireActivity().toString())
         builder.setView(R.layout.dialog_equip)
         builder.setNegativeButton("Unequip") { dialog, _ ->
             onNegativeClick(dialog as AlertDialog)
@@ -27,20 +29,16 @@ class EquipDialog : DialogFragment() {
     }
 
     private fun onPositiveClick(dialog: AlertDialog) {
-        try {
+        if (requireActivity() is BackpackActivity) {
             val intent = Intent().putExtra("click", 1)
             (requireActivity() as BackpackActivity).onActivityResult(1, 1, intent)
-        } finally {
-
         }
     }
 
     private fun onNegativeClick(dialog: AlertDialog) {
-        try {
+        if (requireActivity() is EquipmentActivity) {
             val intent = Intent().putExtra("click", 2)
             (requireActivity() as EquipmentActivity).onActivityResult(2, 1, intent)
-        } finally {
-
         }
     }
 
